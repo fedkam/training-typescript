a0a5fbfecc456ea8460148f413090a705412223b
@fedoskamcha <korolevfedos@yandex.ru>
2020-12-20 19:02:44 +1000

    .

--- 1_types.ts
+++ 1_types.ts
@@ -1,56 +1,41 @@
-
 // boolean
 const isFetching: boolean = true;
 const isLoading: boolean = false;
 
-
-
 // number
-const int: number = 42
+const int: number = 42;
 const float: number = 42.7;
 const num: number = 3e10;
-
-
 
 // string
 const str: string = 'str';
 
-
-
 // array
-const numberArr: number[] = [1, 1, 2];          // 1-й тип объявления с []
-const stringArr: string[] = ['a', 'b', 'c'];    // 1-й
-const numberArr2: Array<number> = [1, 1, 2];    // 2-й тип объявления generic (файл №7)
-
-
+const numberArr: number[] = [1, 1, 2]; // 1-й тип объявления с []
+const stringArr: string[] = ['a', 'b', 'c']; // 1-й
+const numberArr2: Array<number> = [1, 1, 2]; // 2-й тип объявления generic (файл №7)
 
 // tuple - определяют массивы с фикс колвом элем и с определенными type  на каждом
 const fixArray: [string, number] = ['a', 1];
-
-
 
 // any
 let valueAny: any = 1;
 valueAny = '';
 valueAny = [];
 
-
-
 // function
-function sayMyName(name: string): number { return 0 } //обязательно указать тип возвращяемого значения
+function sayMyName(name: string): number {
+    return 0;
+} //обязательно указать тип возвращяемого значения
 sayMyName('');
-
-
 
 // never - либо когда функция return error либо когда функция никогда не завершится(бесконечная рекурсия ....)
 function throwErrorFunc(mess: string): never {
     throw new Error(mess);
 }
 function infinityFunc(mess: string): never {
-    while (true) { }
+    while (true) {}
 }
-
-
 
 // type - собственные типы
 type Login = string;
@@ -60,8 +45,6 @@ type ID = string | number;
 let id1: ID = 124;
 id1 = 'abc';
 
-
 // null and undefined
 let u: undefined = undefined;
 let n: null = null;
-
--- 5_classes.ts
+++ 5_classes.ts
@@ -1,48 +1,45 @@
 // вариант 1
 class Car {
-    readonly model: string
-    readonly numberOfWheels: number = 4
+    readonly model: string;
+    readonly numberOfWheels: number = 4;
 
     constructor(theModel: string) {
-        this.model = theModel       // только в конструкторе класса возможно изменять model
+        this.model = theModel; // только в конструкторе класса возможно изменять model
     }
 }
 
 // вариант 2
 class Car2 {
-    readonly numberOfWheels: number = 4
-    constructor(readonly model: string) { }
+    readonly numberOfWheels: number = 4;
+    constructor(readonly model: string) {}
 }
 
-
-
-// модификаторы
+// модификаторы protected, public, private
 class Animal {
-    protected voice: string = ''        // наследуется
-    public color: string = 'black'
+    protected voice: string = ''; // наследуется
+    public color: string = 'black';
 
     private go() {
-        console.log('go')
+        console.log('go');
     }
 }
 
 class Cat extends Animal {
     public setVoice(voice: string): void {
-        this.voice = voice
+        this.voice = voice;
     }
 }
 let cat = new Cat();
 
-
-
 // абстрактные классы
 abstract class AbstractExaple {
-    abstract render(): void
-    abstract info(): string
+    abstract render(): void;
+    abstract info(): string;
 }
 
 class AppAbstractExaple extends AbstractExaple {
-    render(): void { }
-    info(): string { return ''}
+    render(): void {}
+    info(): string {
+        return '';
+    }
 }
-
